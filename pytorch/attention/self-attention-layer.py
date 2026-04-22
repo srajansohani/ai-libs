abe9723eec90bae25e1d332a634d71c8c8de2f86
srajansohani <99042007+srajansohani@users.noreply.github.com>
2026-04-22 22:26:08 +0530

    Implemented causual/masked attention

--- pytorch/attention/self-attention-layer.py
+++ pytorch/attention/self-attention-layer.py
@@ -1,27 +1,28 @@
 import torch
+import torch.nn as nn
 
-class SelfAttentionLayer:
+class SelfAttentionLayer(nn.Module):
     def __init__(self,input_dim,output_dim):
         self.input_dim = input_dim
         self.output_dim = output_dim
-        self.initialize_weights()
+        self.Wq = nn.Linear(input_dim, output_dim)
+        self.Wk = nn.Linear(input_dim, output_dim)
+        self.Wv = nn.Linear(input_dim, output_dim)
     
-    def initialize_weights(self):
-        self.Wq = torch.randn(self.input_dim, self.output_dim) * (2 / self.input_dim) ** 0.5
-        self.Wk = torch.randn(self.input_dim, self.output_dim) * (2 / self.input_dim) ** 0.5
-        self.Wv = torch.randn(self.input_dim, self.output_dim) * (2 / self.input_dim) ** 0.5
-
-        self.Wq.requires_grad_()
-        self.Wk.requires_grad_()
-        self.Wv.requires_grad_()
+   
     
     def forward(self,input):
-        Q = input @ self.Wq
-        K = input @ self.Wk
-        V = input @ self.Wv
+        Q = self.Wq(input)
+        K = self.Wk(input)
+        V = self.Wv(input)
 
         attention_score = Q @ K 
-        attention_weights = torch.softmax(attention_score/self.input_dim ** 0.5, dim=-1)
+        attention_weights = torch.softmax(attention_score/K.shape[-1] ** 0.5, dim=-1)
         contextual_embeddings = attention_weights @ V
 
         return contextual_embeddings
+    
+
+
+    ##Instead of matrix we can say all of Wq, Wk, and Wv are a linear layered NN
+
